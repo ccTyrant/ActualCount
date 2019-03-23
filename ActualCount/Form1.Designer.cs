5c1d3dcd14722a732e2043314716cbdb2ff1fd09
George Smith (Desktop) <gsmith42a@gmail.com>
2019-03-23 20:01:10 +0000

    Add project files.

---
+++ ActualCount/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ActualCount
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtLink = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.lblPercent = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtLink
+            // 
+            this.txtLink.Location = new System.Drawing.Point(12, 12);
+            this.txtLink.Name = "txtLink";
+            this.txtLink.Size = new System.Drawing.Size(407, 20);
+            this.txtLink.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(425, 10);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(12, 35);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(139, 13);
+            this.lblCount.TabIndex = 2;
+            this.lblCount.Text = "Actual Count of UK citizens:";
+            // 
+            // lblPercent
+            // 
+            this.lblPercent.AutoSize = true;
+            this.lblPercent.Location = new System.Drawing.Point(12, 48);
+            this.lblPercent.Name = "lblPercent";
+            this.lblPercent.Size = new System.Drawing.Size(223, 13);
+            this.lblPercent.TabIndex = 3;
+            this.lblPercent.Text = "Percentage of signatures that are UK citizens:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 85);
+            this.Controls.Add(this.lblPercent);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtLink);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Government petition counter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLink;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Label lblPercent;
+    }
+}
+
---
+++ ActualCount/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace ActualCount
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string content = fileGetContents(txtLink.Text);
+                JObject o = JObject.Parse(content);
+                int total = (int)o.SelectToken("data.attributes.signature_count");
+                foreach (var item in o.SelectToken("data.attributes.signatures_by_country"))
+                {
+                    if ((string)item.SelectToken("name") == "United Kingdom")
+                    {
+                        int value = (int)item.SelectToken("signature_count");
+                        lblCount.Text = "Actual Count of UK citizens: " + value;
+                        double percent = 100 * (double)value / (double)total;
+                        lblPercent.Text = "Percentage of signatures that are UK citizens: " + percent + "%";
+                        break;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Not a valid JSON Link", "Not a valid JSON Link",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        protected string fileGetContents(string fileName) //This function makes the https connection which is used for accessing the Google API
+        {
+            string sContents = string.Empty;
+            string me = string.Empty;
+            try
+            {
+                if (fileName.ToLower().IndexOf("https:") > -1)
+                {
+                    System.Net.WebClient wc = new System.Net.WebClient();
+                    byte[] response = wc.DownloadData(fileName);
+                    sContents = System.Text.Encoding.ASCII.GetString(response);
+
+                }
+                else
+                {
+                    System.IO.StreamReader sr = new System.IO.StreamReader(fileName);
+                    sContents = sr.ReadToEnd();
+                    sr.Close();
+                }
+            }
+            catch { sContents = "unable to connect to server "; }
+            return sContents;
+        }
+    }
+}
